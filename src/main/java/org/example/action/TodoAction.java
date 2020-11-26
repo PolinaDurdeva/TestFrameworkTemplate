9f37b8cceaa9be7b33908a1ee11d790790a984b9
polinadiurdeva <2696668pol>
2020-11-26 16:53:22 +0100

    - added meaningful descriptions for steps

--- src/main/java/org/example/action/TodoAction.java
+++ src/main/java/org/example/action/TodoAction.java
@@ -2,11 +2,12 @@ package org.example.action;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
+import io.qameta.allure.Step;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.example.pageobject.todo.TodoListPage;
 
-public class TodoAction implements Action{
+public class TodoAction implements Action {
 
     private static final Logger LOGGER = LogManager.getLogger(TodoAction.class);
 
@@ -24,27 +25,32 @@ public class TodoAction implements Action{
         return todoListPage.getCompletedTodoList();
     }
 
+    @Step("Add todo item with text {todoText}")
     public TodoAction addNewTodo(String todoText) {
         LOGGER.info("Adding new todo with text - {}", todoText);
         todoListPage.getNewTodoField().setValue(todoText).pressEnter();
         return this;
     }
 
+    @Step("Mark todo item with text {todoText} as completed")
     public TodoAction markAsCompleted(String todoText) {
         LOGGER.info("Marking as completed todo with text - {}", todoText);
         todoListPage.getTodoCheckbox(todoText).click();
         return this;
     }
 
+    @Step("Remove todo item with text {todoText}")
     public TodoAction deleteTodo(String todoText) {
         LOGGER.info("Removing todo with text - {}", todoText);
         todoListPage.getTodoDeleteButton(todoText).click();
         return this;
     }
 
-    public void toggleAllTodo() {
+    @Step("Mark all todo items as completed")
+    public TodoAction toggleAllTodo() {
         todoListPage.getToggleAllButton().shouldBe(Condition.visible);
         todoListPage.getToggleAllButton().click();
+        return this;
     }
 
 }
